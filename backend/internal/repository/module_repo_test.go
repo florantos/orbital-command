33923b812f3f7c88cb4faa592a15504876ae6754
florantos <222972437+florantos@users.noreply.github.com>
2026-03-22 08:15:15 +0800

    feat: add ReadAll to module repo and write tests

--- backend/internal/repository/module_repo_test.go
+++ backend/internal/repository/module_repo_test.go
@@ -46,3 +46,36 @@ func TestModuleRepo_Create_ReturnsErrorOnDuplicateName(t *testing.T) {
 	assert.ErrorIs(t, err, domain.ErrDuplicateModuleName)
 
 }
+
+func TestModuleRepo_ReadAll_ReturnsAllModules(t *testing.T) {
+	pool := testutil.NewTestPool(t)
+	tx := testutil.NewTestTx(t, pool)
+	repo := repository.NewModuleRepo(tx)
+
+	module, err := domain.NewModule("Navigation Array", "Controls navigation systems")
+	require.NoError(t, err)
+	_, err = repo.Create(context.Background(), module)
+	require.NoError(t, err)
+
+	module2, err := domain.NewModule("Navigation Array2", "Controls navigation systems2")
+	require.NoError(t, err)
+	_, err = repo.Create(context.Background(), module2)
+	require.NoError(t, err)
+
+	modules, err := repo.ReadAll(context.Background())
+	require.NoError(t, err)
+
+	assert.Len(t, modules, 2)
+
+}
+func TestModuleRepo_ReadAll_ReturnsEmptyArrayWhenNoModules(t *testing.T) {
+	pool := testutil.NewTestPool(t)
+	tx := testutil.NewTestTx(t, pool)
+	repo := repository.NewModuleRepo(tx)
+
+	modules, err := repo.ReadAll(context.Background())
+	require.NoError(t, err)
+
+	assert.NotNil(t, modules)
+	assert.Len(t, modules, 0)
+}
